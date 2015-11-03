c76fceef05410af078317ff1776e399b6ed83041
Whalen <whalendillon@gmail.com>
2015-11-03 08:52:16 -0700

    Update module 2.3 and 2.4 R scripts
    Edited R scripts to be compatible with knitr::spin function to create R
    markdown files. These are still executable as basic R scripts. This
    commit also adds supporting ArcINFO files.

--- module2/Rscripts/M2.3-modify_raster.r
+++ module2/Rscripts/M2.3-modify_raster.r
@@ -1,92 +1,117 @@
-######## START MODULE 2.3 ########
-#### OBJECTIVE:  
-####   Ensures all raster layers are of same extent, projection, dimension, resolution
-####
-#### Requires data objects from module #1 (module1.RData) & module #2.1 (module2.1.RData)
-####
-#### tested on R versions 3.1.X, 3.2.X
-
-## some libraries ...
+#' ## START MODULE 2.3
+#' 
+#' Objective: Ensure all raster layers are of same extent, projection, dimension, resolution
+#' 
+#' tested on R versions 3.1.X, 3.2.X
+#' 
+#' #### Load packages
+#' 
+#+ load packages
    library(raster)    # fxns: raster, projectRaster, stack
-## some initializations ...
+   library(rgdal)
+#'
+#' Some initializations: set working directory/path to data
+#' Refer to Module 1.1 for help on setting your path and working directory
+#' 
+#+ set working directory
    #path.root <- "~/IALE2015_gisRcourse"
-   path.root <- "~/words/classes/IALE2015_gisRcourse"
-   path.dat <- paste(path.root, "/data", sep = "")
-   setwd(path.dat)
-## some input GIS layers & characteristics from previous modules ...
-##   rasters WGS84: elev1km.wgs84, ndvi1km.wgs84, precdry1km.wgs84, rough1km.wgs84
-##   rasters AEA:   cp.nlcd30m.aea
-##   shapefiles:    soil.rawpoly
-   load("outdata/module1.RData")  # some shapefiles & rasters from module #2.1
-   ls()  # examine
-   load("outdata/module2.1.RData")  # some rasters from module #1
-   ls()  # examine
-## add a new raster for this module ...
-   taveYrAvg1km.wgs84 <- raster("tave_yr_av.img")  # add another .img ... 
+   path.root <- "~/Documents/Intro-Spatial-R/"
+   path.mod <- paste(path.root, "module2", sep = "")
+   setwd(path.mod)
+#'
+#' #### Load some GIS data layers
+#'
+#+ create base raster
+   cp.wgs <- readOGR(dsn = "data", layer = "COP_boundpoly_wgs84")
+   ext.rast <- raster(resolution = 0.008333333, extent(cp.wgs), crs = proj4string(cp.wgs))
+#'   
+#+ load rasters 
+   # in WGS84 projection: 
+   elev1km.wgs84 <- raster("data/elev_1k_wgs.img") 
+   ndvi500m.wgs84 <- raster("data/N200530602133CBR_v1.1.dat")
+   precdry1km.wgs84 <- raster("data/bio14_12.tif")
+   rough1km.wgs84 <- raster("data/rough_1k")
 
-
-## examine existing rasters; NOTE which are different class, resolution, extent, etc
-##   ALL need to match ext.rast (ie. be identical in projection, extent, resolution)
-##   OTHERWISE R won't like you ...
-##     ext.rast            # the base raster to which ALL must match
-##     elev1km.wgs84       # elev 1km raster
-##     ndvi1km.wgs84       # NDVI raster at 1km resolution
-##     precdry1km.wgs84    # precip at 1km raster
-##     rough1km.wgs84      # terrain roughness 1km raster
-##     cp.nlcd30m.aea      # NLCD at 30m raster, AEA
-##     taveYrAvg1km.wgs84  # avg annual temp 1km raster
-
-## compare each raster to base raster => ext.rast; adjust to common projection, resolution, extent
-##   fxn projectRaster => package raster easiest, repeatable method to ensure equal rasters
-##     fxn make all projections, resolutions & extents equal to base raster
-##     applies a bilinear by default; method="ngb" for nearest neighbor
-##
-##   elev1km.wgs84 vs. ext.rast as base raster; NOTE <1 min runtime
-   ext.rast;  elev1km.wgs84   # examine; NOTE wrong extent, dimensions
+   # in Albers Equal Area projection
+   cp.nlcd30m.aea <- raster("data/cpBOX.img")
+   
+   # add a new raster for this module ...
+   taveYrAvg1km.wgs84 <- raster("data/tave_yr_av.img")
+#'
+#+ load soil shapefile. 
+   # This may take a little time
+   soil.rawpoly <- readOGR(dsn = "data", layer = "soils")
+#'
+#' #### Examine existing rasters
+#' 
+#' Note which are different class, resolution, extent, etc. ALL need to match ext.rast (i.e. be identical in projection, extent, resolution), otherwise you won't have accurate calculations and R won't like you ...
+#' 
+#'  - `ext.rast`, the base raster to which ALL must match
+#'  - `elev1km.wgs84`, elev 1km raster
+#'  - `ndvi1km.wgs84`, NDVI raster at 1km resolution
+#'  - `precdry1km.wgs84`, precip at 1km raster
+#'  - `rough1km.wgs84`, terrain roughness 1km raster
+#'  - `cp.nlcd30m.aea`, NLCD at 30m resolution, AEA projection
+#'  - `taveYrAvg1km.wgs84`, mean annual temperature raster, 1km resolution
+#'
+#' Compare each raster to the base raster, i.e. `ext.rast` and adjust to common projection, resolution, and extent as needed.
+#' 
+#' The function `projectRaster` from the `raster` package is the easiest, repeatable method to ensure equal rasters. It makes all projections, resolutions, and extents equal to a base raster, applying a bilinear interpolation by default. Choose another method using `method = "method"` in the function call, e.g. `method = "ngb"` for nearest neighbor.
+#'
+#+ check rasters 
+   # examine elev1km.wgs84 vs. ext.rast; NOTE <1 min runtime
+   ext.rast;  elev1km.wgs84# NOTE different extent, dimensions
    cp.elev <- projectRaster(elev1km.wgs84, ext.rast)
    ext.rast;  cp.elev  # examine; now identical ??
    names(cp.elev) <- "elev";  cp.elev  # apply rename option
 
-##   ndvi1km.wgs84 vs. ext.rast as base raster; NOTE <1 min runtime
-   ext.rast;  ndvi1km.wgs84  # examine; NOTE wrong extent, dimensions, resolution
-   cp.ndvi <- projectRaster(ndvi1km.wgs84, ext.rast)
+   # examine ndvi1km.wgs84 vs. ext.rast; NOTE <1 min runtime
+   ext.rast;  ndvi500m.wgs84# NOTE wrong extent, dimensions, resolution
+   cp.ndvi <- projectRaster(ndvi500m.wgs84, ext.rast)
    ext.rast;  cp.ndvi  # examine; now identical ??
    names(cp.ndvi) <- "ndvi";  cp.ndvi  # apply rename option
 
-##   precdry1km.wgs84 vs. ext.rast as base raster; NOTE <1 min runtime
-   ext.rast;  precdry1km.wgs84  # examine; NOTE wrong extent, dimensions
+   # examine precdry1km.wgs84 vs. ext.rast; NOTE <1 min runtime
+   ext.rast;  precdry1km.wgs84# NOTE wrong extent, dimensions
    cp.precip <- projectRaster(precdry1km.wgs84, ext.rast)
    ext.rast;  cp.precip  # examine; now identical ??
    names(cp.precip) <- "precip";  cp.precip  # apply rename option
 
-##   rough1km.wgs84 vs. ext.rast as base raster; NOTE <1 min runtime
-   ext.rast;  rough1km.wgs84  # examine; NOTE wrong extent, dimensions, projection
+   # examine rough1km.wgs84 vs. ext.rast; NOTE <1 min runtime
+   ext.rast;  rough1km.wgs84# NOTE wrong extent, dimensions, projection
    cp.rough <- projectRaster(rough1km.wgs84, ext.rast)
    ext.rast;  cp.rough  # examine; now identical ??
    names(cp.rough) <- "rough";  cp.rough  # apply rename option 
 
-##   cp.nlcd30m.aea vs. ext.rast as base raster; NOTE ~9 min runtime
-   ext.rast;  cp.nlcd30m.aea  # examine; NOTE wrong extent, dimensions, projection, resolution
-      xx=raster("nlcd1.img")
-      date(); cp.nlcd <- projectRaster(xx, ext.rast, method = "ngb"); date()
-   #cp.nlcd <- projectRaster(cp.nlcd30m.aea, ext.rast, method = "ngb")
-   ext.rast; cp.nlcd         # examine; now identical ??
-   names(cp.nlcd) <- "nlcd"; cp.nlcd  # apply rename option
+   # examine cp.nlcd30m.aea vs. ext.rast; NOTE ~9 min runtime
+   ext.rast;  cp.nlcd30m.aea# NOTE wrong extent, dimensions, projection, resolution
+   system.time(cp.nlcd <- projectRaster(cp.nlcd30m.aea, ext.rast, method = "ngb"))
+   ext.rast; cp.nlcd# examine; now identical ??
+   names(cp.nlcd) <- "nlcd"; cp.nlcd# apply rename option
 
-##   taveYrAvg1kmwgs84 vs. ext.rast as base raster; NOTE <1 min runtime
-   ext.rast;  taveYrAvg1km.wgs84  # examine; NOTE wrong extent, dimension
-   date(); cp.taveYrAvg <- projectRaster(taveYrAvg1km.wgs84, ext.rast); date()
-   ext.rast;  cp.taveYrAvg        # examine; now identical ??
-
-## create a stack of rasters of same projection, extent, resolution, dimension
+   # taveYrAvg1kmwgs84 vs. ext.rast as base raster; NOTE <1 min runtime
+   ext.rast;  taveYrAvg1km.wgs84# NOTE wrong extent, dimension
+   cp.taveYrAvg <- projectRaster(taveYrAvg1km.wgs84, ext.rast)
+   ext.rast;  cp.taveYrAvg# examine; now identical ??
+#'
+#' #### Create raster stack
+#' A raster stack is a set of rasters with the same projection, extent, resolution, and dimension. You can then perform operations on this single object instead of needing to operate on individual rasters.
+#+ stack rasters 
    cp.allvars <- stack(cp.elev, cp.ndvi, cp.precip, cp.rough, cp.taveYrAvg)
-   cp.allvars  # examine
-   
-## convert a raster to a polygon; EXAMPLE cp.soil.1 => phave from Module 2.2
-##   be patient ... ~3 min runtime
+   cp.allvars# examine
+#'   
+#' #### Convert a raster to a polygon 
+#' Example: cp.soil.1 => phave from Module 2.2
+#' Be patient, this will take some time
+#' 
+#+ convert raster to polygon
+   cp.soil.1 <- rasterize(soil.raw, field="phave", ext.rast)
    cp.soil.1P <- rasterToPolygons(cp.soil.1)
    cp.soil.1  # examine raster of soil => phave
    cp.soil.1P  # examine raster2polygon of soil => phave
-####
-######## END MODULE 2.3 ########
 
+#' ## END MODULE 2.3
+#' 
+#' Create R markdown file from R script 
+#+
+   knitr::spin("Rscripts/M2.3-modify_raster.r", knit = F, format = "Rmd")
--- module2/Rscripts/M2.4-alterextent_shapefileraster.r
+++ module2/Rscripts/M2.4-alterextent_shapefileraster.r
@@ -1,124 +1,151 @@
-######## START MODULE 2.4 ########
-#### OBJECTIVE:  
-####   Ensures all raster layers are of same extent, projection, dimension, resolution
-####
-#### Requires data objects from module #1 (module1.RData) & module #2.1 (module2.1.RData)
-####
-#### tested on R versions 3.1.X, 3.2.X
-
-## some libraries ...
+#' ## START MODULE 2.4
+#' Objective: Edit extent, projection, dimension, resolution of raster layers or shapefiles
+#' 
+#' tested on R versions 3.1.X, 3.2.X
+#' 
+#' Load packages and set working directory
+#+ load packages
    library(rgdal)     # fxns: readOGR, writeOGR
    library(raster)    # fxns: crop, mask
-## some initializations ...
-   #path.root <- "~/IALE2015_gisRcourse"
-   #path.dat="E:/IALE2015_gisRcourse/data"
-   path.root <- "~/words/classes/IALE2015_gisRcourse"
-   path.dat <- paste(path.root, "/data", sep = "")
-   setwd(path.dat)
-
-   
-####
-## polygon crop of polygon
-##   import shapefile to be altered 
-##   CP study area; the "mask" for cropping
-   cp.region <- readOGR(dsn = ".", layer = "COP_boundpoly_aea")
+#  set path
+   path.root <- "~/Documents/Intro-Spatial-R/"
+   path.mod <- paste(path.root, "module2", sep = "")
+   setwd(path.mod)
+#'
+#' ### Crop a polygon file to another polygon
+#' Import shapefile to be altered
+#' 
+#+ import shapefile 
+#  CP study area; the "mask" for cropping
+   cp.region <- readOGR(dsn = "data", layer = "COP_boundpoly_aea")
    class(cp.region)  # examine class
-##   north American states/provinces to be altered
-   states <- readOGR(dsn = ".", layer = "na_states_aea")
-   class(states)  # examine class
 
-## subset states (UT, AZ, CO, NM) in Colorado Plateau region 
-##   NOTE: str() to determine shapefile characteristics
-##     here, states$CODE returns all state-based codes
-##   NOTE: rgdal squirrelly; must exclude NA or crashes if NA present
+#  North American states/provinces to be altered
+   states <- readOGR(dsn = "data", layer = "na_states_aea")
+   class(states)  # examine class
+#'
+#' Subset states (UT, AZ, CO, NM) in Colorado Plateau region 
+#'  - NOTE: str() to determine shapefile characteristics
+#'     here, states$CODE returns all state-based codes
+#'  - NOTE: rgdal can be squirrelly, requiring NAs to be excluded, it crashes if NA present
+#'
+#+ subset states
    head(states@data)  # examine shapepoly data
    states$CODE  # state codes
-   cp.states <- states[states$CODE == "UT" & !is.na(states$CODE) | 
-                       states$CODE == "CO" & !is.na(states$CODE) | 
-                       states$CODE == "NM" & !is.na(states$CODE) | 
-	                   states$CODE == "AZ" & !is.na(states$CODE), ]
+   cp.states <- states[states$CODE == "UT" | states$CODE == "CO" | 
+                             states$CODE == "NM" | states$CODE == "AZ", ]
+   
+#    cp.states <- states[states$CODE == "UT" & !is.na(states$CODE) | 
+#                        states$CODE == "CO" & !is.na(states$CODE) | 
+#                        states$CODE == "NM" & !is.na(states$CODE) | 
+# 	                   states$CODE == "AZ" & !is.na(states$CODE), ]
    cp.states@data  # examine subsetted states
-## giggle plots
-   plot(states)  # all states
-   plot(cp.states, add = T, col = "blue")  # the subsetted states
-   plot(cp.region, add = T, col = "red")  # the study region
-## crop states to the CP region   
-   cp.statesCROP=crop(cp.states,cp.region)
+#'
+#+ giggle plots
+   plot(states)# all of North America
+   plot(cp.states, add = T, col = "blue")# the subsetted states
+   plot(cp.region, add = T, col = "red")# the study region, taking long time
+#'
+#' Crop states to the CP region
+#' 
+#+ crop states  
+   cp.statesCROP <- crop(cp.states,cp.region)
    plot(cp.states)  # giggle plot
 ## output shapefile if desired; examine in ArcGIS if desired
-   #writeOGR(cp.statesCROP, dsn = "./outdata", layer = "cp.statesCROP", 
-   #  morphToESRI = T, driver = "ESRI Shapefile")
-####
-
-####
-## polygon crop of raster
-## a 2-step process; easy to construct as fxn if desired
-##   import a raster to crop
-   tave <- raster("tave_yr_av.img")
+   # writeOGR(cp.statesCROP, dsn = "./outdata", layer = "cp.statesCROP", 
+   # morphToESRI = T, driver = "ESRI Shapefile")
+#'
+#' ### Polygon crop of raster
+#' This is a 2-step process that is easy to construct as function if desired.
+#' 
+#+ import raster
+   tave <- raster("data/tave_yr_av.img")
    tave  # examine raster
-   cp.region  # examine crop polygon (from above)
-## NOTE diff projections; must convert 1 to match other (see Module 2.2)
+   summary(cp.region)  # examine crop polygon (from above)
+#'   
+#' Note the different projections; must convert 1 to match other (see Module 2.2)
+#' 
+#+ transform to match projections
    cp.regionWGS <- spTransform(cp.region, CRS = CRS(projection(tave)))
    cp.regionWGS  # now same projection as tave
+   
 ## giggle plots; goal is crop of tave to match red in plot
    plot(tave)
    plot(cp.regionWGS, add = T, col = "red")
-## step #1;  fxn mask() w/CP polygon as mask applied to tave
+#'   
+#' Step #1; `mask()` function w/CP polygon as the mask applied to tave
+#' 
+#+ mask tave
    taveCROP.1 <- mask(tave, cp.regionWGS)
+   
 ## giggle plots; LOL here .....
-   plot(taveCROP.1)  # NOTE extent is extent of tave
+   plot(taveCROP.1)# NOTE extent is extent of tave
    plot(extent(taveCROP.1), add = T)
-## step #2; fxn crop() to subset masked tave to extent of CP region
+#'   
+#' Step #2; `crop()` function to subset the masked tave to the extent of the CP region
+#' 
+#+ crop tave to CP region
    taveCROP.2 <- crop(taveCROP.1, cp.regionWGS)
+   
 ## giggle plots ...
    plot(taveCROP.2)
    plot(cp.regionWGS, add = T)
+   
 ## outfile cropped raster if desired
    #writeRaster(taveCROP.2, filename = "outdata/cp.taveCROP2.img", format = "HFA")
-####
-
-####
-## raster crop of raster
-## assume raster of region to serve as crop; here cp.rast (see Module 2.2)
-   load("outdata/cp.rast.RData")  # raster of CP region
+#'
+#' ### Crop raster using another raster
+#' Assume raster of region to serve as crop; here cp.rast (see Module 2.2)
+#'
+#+ crop raster by raster 
+   ext.rast <- raster(resolution = 0.008333333, extent(cp.regionWGS))
+   cp.rast <- rasterize(cp.regionWGS, field = "Id", ext.rast)
    cp.rast  # examine
-## raster to be cropped; same projection /resolution as cp.rast?  if not see Module 2.3
+   
+## raster to be cropped
+## same projection/resolution as cp.rast?  if not see Module 2.3
    tave  # examine
+   
 ## some giggle plots; crop goal is red
    plot(tave)
    plot(cp.rast, add = T, col = "red")
+   
 ## crop tave raster w/cp.rast raster
    taveCROP.3 <- crop(tave, cp.rast)
-## outfile cropped raster if desired
+   
+## write out file of cropped raster if desired
    #writeRaster(taveCROP.3, filename = "outdata/cp.taveCROP3.img", format = "HFA")
-## giggle plots; NOTE that crop is cp.rast extent, not just CP region
+   
+## giggle plots; NOTE that crop is the cp.rast extent, not just CP region
    plot(taveCROP.3)
    plot(cp.rast, add = T, col = "red")
-## mulitply rasters if desire cp.rast only; otherwise default is rectangle of extent
-   taveCROP.4 <- cp.rast * taveCROP.3  # NOTE; cp.rast has value => NA outside of CP per se
+   
 ## giggle plots
    par(mfrow = c(1, 2))
    plot(taveCROP.3)
    plot(cp.regionWGS, add = T, border = "black")
-   plot(taveCROP.4)
-   plot(cp.regionWGS, add = T, border = "black")
+
 ## outfile cropped raster if desired
-   #writeRaster(taveCROP.4, filename = "outdata/cp.taveCROP4.img", format = "HFA")
-####
+   #writeRaster(taveCROP.3, filename = "outdata/cp.taveCROP3.img", format = "HFA")
+#'
+#' Crop a polygon to a raster
+#'
+#+ load shapefile 
+   soil <- readOGR(dsn = "data", layer = "soils")  # be patient here ... ~3 min runtime
+   soil # examine polygon
+   cp.rast # examine raster; both have same projection?  if not see Module 2.2/2.3
    
-####
-## raster crop of polygon
-   soil <- readOGR(dsn = ".", layer = "soils")  # be patient here ... ~3 min runtime
-   soil  # examine polygon
-   cp.regionWGS  # examine raster; both have same projection?  if not see Module 2.2/2.3
 ## crop polygon w/extent of raster
-   soilCROP.1=crop(soil,extent(cp.regionWGS))
+   soilCROP.1 <- crop(soil,extent(cp.rast))
 ## giggle plots
    par(mfrow = c(1, 2))
    plot(soil)
    plot(cp.regionWGS, add = T, col = "red")
    plot(soilCROP.1)
-   plot(cp.regionWGS, add = T, col = "red")
-####
-######## END MODULE 2.4 ########
-
+   plot(cp.rast, add = T, col = "red")
+#'
+#' ## END MODULE 2.4
+#' 
+#' Create R markdown file from R script   
+#+
+   knitr::spin("Rscripts/M2.4-alterextent_shapefileraster.r", knit = F, format = "Rmd")
